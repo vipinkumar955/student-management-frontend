52209e5590edef538a17435de02a3f82df0d4a79
vipin <vipinrajput7654@gmail.com>
2026-04-13 07:58:37 +0530

    fix api

--- src/api.jsx
+++ src/api.jsx
@@ -3,13 +3,18 @@ import axios from "axios";
 const API = axios.create({
   baseURL: `${import.meta.env.VITE_API_URL}/api/`,
 });
-
 // Add token in every request
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem("access");
-  if (token) {
+
+  if (
+    token &&
+    !config.url.includes("login") &&
+    !config.url.includes("register")
+  ) {
     config.headers.Authorization = `Bearer ${token}`;
   }
+
   return config;
 });
 
